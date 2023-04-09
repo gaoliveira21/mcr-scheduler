4a06d6c9329f92d19acc304f7ab7ad6fa71a6283
Gabriel José de Oliveira <gaoliveira211999@gmail.com>
2023-04-09 10:43:37 -0300

    feat(users): encrypt password

--- apps/core/src/users/users.module.ts
+++ apps/core/src/users/users.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { EncryptModule } from '@lib/encrypt';
 
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { UsersRepository } from './users.repository';
 
 @Module({
+  imports: [EncryptModule],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
 })
--- apps/core/src/users/users.service.ts
+++ apps/core/src/users/users.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { EncryptService } from '@lib/encrypt';
+
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly usersRepository: UsersRepository) {}
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private readonly encryptService: EncryptService,
+  ) {}
 
   async create({ password, ...data }: CreateUserDto) {
-    const user = this.usersRepository.create({
+    const encryptedPassword = await this.encryptService.encrypt(password);
+
+    const user = await this.usersRepository.create({
       ...data,
-      password,
+      password: encryptedPassword,
     });
 
     return user;
