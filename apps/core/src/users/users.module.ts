5736cf61b18871301c824c8913c2399c479f941c
Gabriel José de Oliveira <gaoliveira211999@gmail.com>
2023-04-09 11:11:10 -0300

    feat(prisma): setup prisma

--- apps/core/src/users/users.module.ts
+++ apps/core/src/users/users.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { EncryptModule } from '@lib/encrypt';
+import { PrismaClientModule } from '@lib/prisma-client';
 
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { UsersRepository } from './users.repository';
 
 @Module({
-  imports: [EncryptModule],
+  imports: [EncryptModule, PrismaClientModule],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
 })
--- apps/core/src/users/users.repository.ts
+++ apps/core/src/users/users.repository.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClientService } from '@lib/prisma-client';
 
 import {
   CreateUserInput,
@@ -7,13 +8,19 @@ import {
 
 @Injectable()
 export class UsersRepository {
+  constructor(private readonly prisma: PrismaClientService) {}
+
   async create(createUserInput: CreateUserInput): Promise<CreateUserOutput> {
+    const user = await this.prisma.user.create({
+      data: { ...createUserInput },
+    });
+
     return {
-      id: '1',
-      name: createUserInput.name,
-      email: createUserInput.email,
-      createdAt: new Date(),
-      updatedAt: new Date(),
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
     };
   }
 }
